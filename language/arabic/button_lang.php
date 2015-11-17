8b5ac2829996929f7d299561c28c610133ee9510
francisalfanta <francisalfanta@gmail.com>
2015-11-17 09:13:32 +0400

    Added Arabic, Hindi

---
+++ language/arabic/button_lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['btn_search']    = 'بحث';
+$lang['btn_free']      = 'حر';
+$lang['btn_ordernow']  = 'اطلب الان';
+$lang['btn_send']      = 'إرسال';
+$lang['btn_subscribe'] = 'الاشتراك';
+$lang['btn_booknow']  = 'Book Now';
+$lang['btn_submit']	  = 'Submit';
+$lang['btn_live_preview'] = 'Live Preview';
+$lang['btn_view']	  	= 'View';
+$lang['btn_order']	  	= 'Order';
+$lang['btn_close']		= 'Close';
